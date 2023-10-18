f43a73aaa95d2427ecdefdda4e3ea407dca83e94
David Nguyen <dnguye05@g.uafs.edu>
2023-10-18 15:40:52 -0500

    Update files

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ int main() {
 	int a = 2;
 	int b = 4;
 	int total = a + b;
-    printf("a: %d, b: %d\n", a, b);
+	printf("a: %d, b: %d\n", a, b);
 	printf("Hello World! Total: %d\n", total);
 	return 0;
 }
